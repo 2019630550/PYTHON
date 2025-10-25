4e38e0c267d353660c9875ed6d7c6a130fd8f9d7
brenda <55.veruete.hernandez.bryan.dav@gmail.com>
2025-10-24 19:28:03 -0600

    Estos son todos los operadores logicos

---
+++ operaciones_aritmeticas.py
@@ -0,0 +1,26 @@
+#Operadores aritméticos 
+a = 10
+b = 3
+x = 20
+y = 7
+edad = 25
+es_estudiante = True
+
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+print("División entera:", a // b)
+print("Módulo:", a % b)
+print("Potencia:", a ** b)
+
+#Operadores de comparación
+print(x > y)
+print(x == y)
+print(x != y)
+
+#Operadores lógicos
+print(edad > 18 and es_estudiante)
+print(edad < 18 or es_estudiante)
+print(not es_estudiante)
